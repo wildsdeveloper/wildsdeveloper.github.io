0de245a9992b301a3f9ea82c1ccf09a44bfd2200
Wilder Rincon <wilder.rincon@cun.edu.co>
2024-01-29 20:20:32 -0500

    refactor: solution lcp performance title h1 hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,17 +9,17 @@ const Hero = () => {
       <div
         className={`absolute inset-0 sm:top-[50px] md:top-[70px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-16 md:mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-16 md:mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div className="mt-5 md:mt-2">
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Wilder Rincon</span>
+          <h1 className="font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-16 md:mt-5 tracking-tight">
+            Hi, I'm <span className="text-[#915EFF]">Wilder Rincon</span>
           </h1>
           <p className={`${styles.heroSubText} text-white-100`}>
-            I develop modern websites, user <br className='sm:block hidden' />
+            I develop modern websites, user <br className="sm:block hidden" />
             interfaces and web applications
           </p>
         </div>
@@ -27,9 +27,9 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -39,7 +39,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
