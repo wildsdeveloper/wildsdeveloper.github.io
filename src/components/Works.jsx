11a36b4a83b81285732f685f0ef5277a799e9cf0
Wilder Rincon <wilder.rincon@cun.edu.co>
2024-02-06 10:34:00 -0500

    refactor: optimize charge fonts options preconnect

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -89,7 +89,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-20 flex flex-wrap gap-4'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
