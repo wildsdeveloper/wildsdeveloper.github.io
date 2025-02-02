ea77fc215f63fd77b7231cc9d88a0cb9e3e9ddd2
Wilds Rincon <wilder.rincon@cun.edu.co>
2025-02-02 13:05:02 -0500

    fix: Change file CV hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
         >
           <a
             download="WildsRincon-Resume.pdf"
-            href="/files/CV_Wilder_Rincon_US.pdf"
+            href="/files/CV_WILDER_RINCON_2024.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
           >
